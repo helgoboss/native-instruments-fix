2457e8fdd5548609cc5fb34ad259d59895fa9bca
Benjamin Klum <bklum@avantgarde-labs.de>
2015-09-05 20:59:26 +0200

    Make Battery 4 see the factory library when it's started

--- helgoboss-ni-fix-battery-4.bat
+++ helgoboss-ni-fix-battery-4.bat
@@ -1,1 +1,3 @@
 call .\helgoboss-ni-fix "Battery 4" "Battery 4______"
+
+reg copy "HKEY_LOCAL_MACHINE\SOFTWARE\Native Instruments\Battery 4 Factory Library" "HKEY_LOCAL_MACHINE\SOFTWARE\NI Battery 4______\Battery 4 Factory Library" /s /f
